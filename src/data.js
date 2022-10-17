9bde90173c6b05b3012084bf9e1dcb47c1a5d123
Aditi <95167364+aditi-hinge@users.noreply.github.com>
2022-10-17 13:26:42 +0900

    Date file with menu info

---
+++ src/data.js
@@ -0,0 +1,27 @@
+export const menu = [
+  {
+    id: 1,
+    name: "Coffee",
+    price: "280",
+  },
+  {
+    id: 2,
+    name: "Tea",
+    price: "380",
+  },
+  {
+    id: 3,
+    name: "Coke",
+    price: "400",
+  },
+  {
+    id: 4,
+    name: "Milk",
+    price: "500",
+  },
+  {
+    id: 5,
+    name: "Beer",
+    price: "580",
+  },
+];
